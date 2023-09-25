3fe303ceee1d6249096d4d556f6ae3dec7bd8bd0
Diego Rodriguez <godiedri@gmail.com>
2023-09-25 15:41:16 +0200

    crud, mongodb

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import postsRoutes from "./routes/posts.routes.js"
-
+import { connectDB } from './db.js'
+import { PORT } from "./config.js";
 const app = express()
+connectDB()
 
 app.use(postsRoutes)
-app.listen(3000)
-console.log('server is running port', 3000)
+app.listen(PORT)
+console.log('server is running port', PORT)
 
