f3b7864f56f8986ae762ef17105b065fe2c3c013
Diego Rodriguez <godiedri@gmail.com>
2023-10-10 12:28:03 +0200

    avance error handler

--- Server/controllers/posts.controllers.js
+++ Server/controllers/posts.controllers.js
@@ -12,8 +12,19 @@ export const createPost = async (req, res) => {
     return res.json(newPost)
 }
 
-export const updatePost = (req, res) => res.send('Updating a post')
+export const updatePost = async (req, res) => {
+    const post = await Post.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    return res.send('recived')
+}
 
-export const deletePost = (req, res) => res.send('Deleting a post')
+export const deletePost = async (req, res) => {
+    const postRemoved = await Post.findByIdAndDelete(req.params.id)
+    if (!postRemoved) return res.sendStatus(404)
+    return res.sendStatus(204)
+}
 
-export const getPost = (req, res) => res.send('Getting a post')
+export const getPost = async (req, res) => {
+    const post = await Post.findById(req.params.id)
+    if (!post) return res.sendStatus(404)
+    return res.json(post)
+}
